72d0fd03b19c98a7ec841c7096366a07b87530f3
fjrd12 <fjrd12@gmail.com>
2020-05-29 21:22:25 +0000

    Fragment inclusion

--- FirstMvc/webapp/controller/App.controller.js
+++ FirstMvc/webapp/controller/App.controller.js
@@ -1,23 +1,6 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller","sap/m/MessageBox","sap/m/MessageToast"
-], function (Controller,MessageBox,MessageToast) {
-   "use strict";
-   return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-      onShowHello : function () {
-         // show a native JavaScript alert
-//         alert("Hello World");
-         //show an sapui5 alert
-         var oModel = this.getView().getModel();
-         var name = oModel.getProperty("/recipient/name");
-         var lastname = oModel.getProperty("/recipient/lastname");
-         MessageBox.information("The record of " + name + " " + lastname + " is registered");
-
-         // read msg from i18n model
-         var oBundle = this.getView().getModel("i18n").getResourceBundle();
-         var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-         // show message
-         MessageToast.show(sMsg);
-      }
-   });
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {});
 });
---
+++ FirstMvc/webapp/controller/EntryPanel.controller.js
@@ -0,0 +1,42 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller", "sap/m/MessageBox", "sap/m/MessageToast", "sap/ui/core/Fragment"
+], function (Controller, MessageBox, MessageToast, Fragment) {
+	"use strict";
+	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+		onShowHello: function () {
+			// show a native JavaScript alert
+			//         alert("Hello World");
+			//show an sapui5 alert
+			var oModel = this.getView().getModel();
+			var name = oModel.getProperty("/recipient/name");
+			var lastname = oModel.getProperty("/recipient/lastname");
+			MessageBox.information("The record of " + name + " " + lastname + " is registered");
+
+			// read msg from i18n model
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+			// show message
+			MessageToast.show(sMsg);
+		},
+		onOpenDialog: function () {
+			
+			var oView = this.getView();
+			// create dialog lazily
+			if (!this.byId("helloDialog")) {
+				// load asynchronous XML fragment
+				Fragment.load({
+					id: oView.getId(),
+					name: "sap.ui.demo.walkthrough.view.HelloDialog"
+				}).then(function (oDialog) {
+					// connect dialog to the root view of this component (models, lifecycle)
+					oView.addDependent(oDialog);
+					oDialog.open();
+				});
+			} else {
+				this.byId("helloDialog").open();
+			}
+			
+		}
+	});
+});
